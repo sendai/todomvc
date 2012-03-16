3207de295572ca48371c2f53a97341c675153505
Paul <logrado@gmail.com>
2012-03-15 19:35:55 -0500

    Exploring how events work.

--- src-cljs/todomvc/todomvc.cljs
+++ src-cljs/todomvc/todomvc.cljs
@@ -12,5 +12,11 @@
     (dom/set-style! node :display "block")
     (dom/set-style! node :display "none")))
 
+;; some examples of what we can do with events
 (listen! (sel "#instructions") :click (fn [] (js/alert "hi")))
 (listen! (sel "#credits") :click (toggle-display (sel "#main")))
+(listen! (sel "#todoapp h1") :click #(dom/append! (sel "body") "<p>hi, there!</p>"))
+(listen! (sel "#new-todo") :focus #(dom/set-style! (sel "#todoapp footer") :display "block"))
+(listen! (sel "#new-todo") :blur #(dom/set-style! (sel "#todoapp footer") :display "none"))
+
+(dom/set-text! (dom/by-id "todo-count") "3 todo items")
