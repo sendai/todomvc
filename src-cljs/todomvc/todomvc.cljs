c212e261824ece341ce939b6245016d7f4a0fc9a
Paul <logrado@gmail.com>
2012-03-19 17:47:21 -0500

    Can read and write from localStorage now.

--- src-cljs/todomvc/todomvc.cljs
+++ src-cljs/todomvc/todomvc.cljs
@@ -1,9 +1,11 @@
 (ns tm1.todomvc
        (:require [clojure.browser.repl :as repl]
-                 [domina :as dom])
+                 [domina :as dom]
+                 [goog.dom :as gdom])
        (:use [domina.css :only [sel]]
              [domina.xpath :only [xpath]]
-             [domina.events :only [listen!]]))
+             [domina.events :only [listen!]]
+             [cljs.reader :only [read-string]]))
 
 (repl/connect "http://localhost:9000/repl")
 
@@ -20,3 +22,21 @@
 (listen! (sel "#new-todo") :blur #(dom/set-style! (sel "#todoapp footer") :display "none"))
 
 (dom/set-text! (dom/by-id "todo-count") "3 todo items")
+
+(defn get-todos []
+  (read-string (.getItem (.-localStorage js/window) "todo-clojurescript")))
+
+(defn save-todos [todos]
+  (let [todos-string (pr-str todos)]
+    (.setItem (.-localStorage js/window) "todo-clojurescript" todos-string)))
+
+(defn add-todo [text]
+  (let [todos (get-todos)]
+    (save-todos (conj todos text))))
+
+(doseq [t (get-todos)]
+  (dom/append! (sel "body") (str "<p>" t "</p>")))
+
+;(save-todos ["task 1" "task 2"])
+
+(listen! (sel "#new-todo-form") :submit #(add-todo (dom/value (sel "#new-todo"))))
