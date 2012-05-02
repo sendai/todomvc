fc1226b849fbb9413de9778b81b0a1d7e174aca6
Paul Ewing <paul.ewing.ctr@hpcmo.hpc.mil>
2012-05-02 17:44:40 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject todomvc "0.1.0-SNAPSHOT"
+(defproject todomvc "0.1.1-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [goog-jar "1.0.0"]
                            [noir "1.2.1"]]
-            :plugins [[lein-cljsbuild "0.1.2"]
+            :plugins [[lein-cljsbuild "0.1.8"]
                       [lein-git-deps "0.0.1-SNAPSHOT"]]
             :git-dependencies [["https://github.com/levand/domina.git"
-                               "1fcbfa2375f360cb1167f9de7d928eb5b8d64514"]]
+                               "753c18e4a012164940904b375b3cf29c77771bef"]]
             :cljsbuild
             {:builds
             [{:source-path "src-cljs",
